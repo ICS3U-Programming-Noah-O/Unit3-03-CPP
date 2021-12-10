365e32da9bc5f5e89a56e0d440c7cab41ffeb90d
Noah-234 <noah.ouellette@stu.ocsb.ca>
2021-12-10 18:26:37 +0000

    Created and completed a program that generates a random number and then allows the user to guess it

---
+++ randNumGuess.cpp
@@ -0,0 +1,45 @@
+// Copyright (c) 2021 Noah Ouellette All rights reserved.
+//
+// Created by: Noah Ouellette
+// Created on: Dec. 9, 2021
+// This program allows a user to guess a number between 0
+// and 9 with a randomly generated number
+#include <iostream>
+#include <iomanip>
+#include <random>
+
+
+// declare constants
+int CONST_NUM = 8;
+
+
+int main() {
+  // Generate random number
+  int randomNumber;
+
+  std::random_device rseed;
+
+  std::mt19937 rgen(rseed());  // mersenne_twister
+
+  std::uniform_int_distribution<int> idist(0, 9);
+
+  randomNumber = idist(rgen);
+
+  // get input from user
+  int userNumber;
+  std::cout << "I have picked a number between 0 and 9!\n";
+  std::cout << "Guess my number!.\n";
+  std::cout << " \n";
+  std::cout << "Enter your guess: ";
+  std::cin >> userNumber;
+
+  // process/ouput
+  if (userNumber == randomNumber) {
+      std::cout << "You guessed correctly!.\n";
+  } else {
+      std::cout << "Sorry that is incorrect. " << "The answer was ";
+      std::cout << randomNumber << ". Try again!\n";
+      std::cout << " \n";
+      main();
+  }
+}
